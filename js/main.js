33043805f3a0a4be17029bec06d45dc399217c13
Lee Bock <lbock@esri.com>
2020-12-28 11:59:07 -0500

    moving story text and picture info to csv files.

--- js/main.js
+++ js/main.js
@@ -21,17 +21,74 @@ function(
 	var _view;
 	var _lastScrollTop = 0; // used to track whether scroll is forward or reverse
 
-	const TICK_LIMIT = 440;	
+	const TICK_LIMIT = 440;
 	const URL_GPS = "https://services.arcgis.com/nzS0F0zdNLvs7nc8/arcgis/rest/services/old_rg_sectionz/FeatureServer/0";
 	const URL_JSON_WAYPOINTS = "resources/waypoints.json";
+	const URL_TEXT = "resources/text.csv";
+	const URL_PICTURES = "resources/pictures.csv";
+	
 	var ANIMATIONS;
+	var HEADERS;
+	var TEXT;
+	var PICTURES;
 	
 	$(document).ready(function() {
 	
 		$.getJSON(URL_JSON_WAYPOINTS, function(data) {ANIMATIONS = data; finish();});
+		Papa.parse(URL_TEXT, {
+			header: true,
+			download: true,
+			complete: function(results) {
+				HEADERS = $.map(
+					$.grep(
+						results.data, 
+						function(value){return value.Type === "header";}
+					), 
+					function(value){return value.Text;}
+				);
+				TEXT = $.map(
+					$.grep(
+						results.data, 
+						function(value){return value.Type === "text-content";}
+					), 
+					function(value){return value.Text;}
+				);
+				finish();
+			}
+		});				
+		Papa.parse(URL_PICTURES, {
+			header: true,
+			download: true,
+			complete: function(results) {PICTURES = results.data; finish();}
+		});		
 		
 		function finish()
 		{
+			if (!ANIMATIONS || !TEXT || !PICTURES) {
+				return;
+			}
+
+			$.each(
+				$("section#intro h3, section#conclusion h3, section#footer h4"),
+				function(idx, value){$(value).html(HEADERS[idx]);}
+			);
+			$.each(
+				$("p.text-content"), 
+				function(idx, value){$(value).html(TEXT[idx]);}
+			);
+			$.each(
+				$("div.picture-frame"),
+				function(idx, value) {
+					$("<div>")
+						.addClass("picture")
+						.css("background-image", "url('"+PICTURES[idx].URL+"'")
+						.appendTo($(value));
+					$("<span>")
+						.addClass("picture-caption")
+						.html(PICTURES[idx].Caption)
+						.appendTo($(value));
+				}
+			);
 
 			window.onbeforeunload = function (){window.scrollTo(0, 0);};
 			$(".banner a:nth-of-type(2)").click(
